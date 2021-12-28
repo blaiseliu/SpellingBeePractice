b34c21faf29373b9dcd60447294f25797a5477ce
Blaise <blaise.liu@hotmail.com>
2021-12-28 08:44:07 -0500

    Complete word list

---
+++ src/SpellingBeePractice.StaticApp/Pages/Page24.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SpellingBeePractice.StaticApp.Models;
+using SpellingBeePractice.StaticApp.Services;
+
+namespace SpellingBeePractice.StaticApp.Pages
+{
+    public partial class Page24
+    {
+        private IEnumerable<WordsExportViewModel> Words { get; set; }
+
+        [Inject]
+        public IWordsDataServices WordsDataServices { get; set; }
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+
+        private void Collapse(string elementID)
+        {
+            JSRuntime.InvokeAsync<object>("Collapse", elementID);
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            var words = await WordsDataServices.GetWordsFromCollection("page24");
+            Words = words;
+        }
+    }
+}
